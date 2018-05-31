fad71994e199dc360fe3ecaaefb2ca6e813e161a
hsw919 <hunterwoody@gmail.com>
2018-05-30 21:38:16 -0400

    Have the more information working

--- freeshelf/src/App.js
+++ freeshelf/src/App.js
@@ -7,23 +7,26 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      moreInfo: false,
-      books: books
+      books: books,
+      moreInfo: Array(books.length).fill(false)
     }
   }
-  expand (book) {
-    this.state.books.forEach(book => {
-      this.setState({
-        moreInfo: !this.state.moreInfo
-      })
-    })
+  expand (book, idx) {
+    const newState = this.state.moreInfo[idx] = !this.state.moreInfo[idx]
+    const info = document.querySelectorAll('#moreInfo')
+    const infoButton = document.querySelectorAll('.info-button')
+    if (newState === true) {
+      console.log(infoButton[idx])
+      infoButton[idx].innerHTML = 'Less information'
+      info[idx].classList.remove('hidden')
+    } else {
+      infoButton[idx].innerHTML = 'More information'
+      info[idx].classList.add('hidden')
+    }
   }
 
   render () {
-    // console.log(this.state.books)
-    console.log(this.state.moreInfo)
     const books = this.state.books
-    // console.log(books)
     return (
       <div className='App'>
         {books.map((book, idx) => (
@@ -34,7 +37,13 @@ class App extends Component {
             <div className='img-container'>
               <img className='book__img' src={book.coverImageUrl} />
             </div>
-            <h4 onClick={() => this.expand(book)} className=''>More information</h4>
+            <button onClick={() => this.expand(book, idx)} className='info-button' key={idx}>More information</button>
+            <div className='hidden' id='moreInfo'>
+              <p><span>URL</span>: <a href={book.url}>{book.url}</a></p>
+              <p><span>Publisher</span>: {book.publisher}</p>
+              <p><span>Publication Date</span>: {book.publicationDate}</p>
+              <p><span>Full Description</span>:<br />{book.detailedDescription}</p>
+            </div>
           </div>
         ))}
       </div>
